026bcad7015eca908983c6dc8a2c586434922494
Diego <105765223+0xfuturistic@users.noreply.github.com>
2023-12-07 18:12:43 +0100

    contracts-bedrock: update tests to support changes to L1ERC721Bridge, L2ERC721Bridge
    L1ERC721Bridge, L2ERC721Bridge had their immutables moved to storage

--- packages/contracts-bedrock/test/vendor/Initializable.t.sol
+++ packages/contracts-bedrock/test/vendor/Initializable.t.sol
@@ -131,7 +131,9 @@ contract Initializer_Test is Bridge_Initializer {
         contracts.push(
             InitializeableContract({
                 target: address(l1ERC721Bridge),
-                initCalldata: abi.encodeCall(l1ERC721Bridge.initialize, (superchainConfig)),
+                initCalldata: abi.encodeCall(
+                    l1ERC721Bridge.initialize, (l1CrossDomainMessenger, address(l2ERC721Bridge), superchainConfig)
+                    ),
                 initializedSlotVal: deploy.loadInitializedSlot("L1ERC721Bridge", true)
             })
         );
@@ -139,7 +141,7 @@ contract Initializer_Test is Bridge_Initializer {
         contracts.push(
             InitializeableContract({
                 target: address(l2ERC721Bridge),
-                initCalldata: abi.encodeCall(l2ERC721Bridge.initialize, ()),
+                initCalldata: abi.encodeCall(l2ERC721Bridge.initialize, (l2CrossDomainMessenger, address(l1ERC721Bridge))),
                 initializedSlotVal: deploy.loadInitializedSlot("L2ERC721Bridge", false)
             })
         );
