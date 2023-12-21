742569b33bb73a0a01207ed3370838636450e5ac
Hamdi Allam <hamdi.allam97@gmail.com>
2023-12-21 10:48:17 -0800

    update toml to devnet default. no default in config deser, fail if not set

--- indexer/config/devnet.go
+++ indexer/config/devnet.go
@@ -17,19 +17,19 @@ func DevnetPreset() (*Preset, error) {
 		return nil, err
 	}
 
-	root, err := findMonorepoRoot(cwd)
+	root, err := findDevnetAllocs(cwd)
 	if err != nil {
 		return nil, err
 	}
 
 	devnetFilepath := filepath.Join(root, ".devnet", "addresses.json")
 	if _, err := os.Stat(devnetFilepath); errors.Is(err, fs.ErrNotExist) {
-		return nil, err
+		return nil, fmt.Errorf(".devnet/addresses.json not found. `make devnet-allocs` in monorepo root: %w", err)
 	}
 
 	content, err := os.ReadFile(devnetFilepath)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("unable to read .devnet/addressees.json")
 	}
 
 	var l1Contracts L1Contracts
@@ -38,34 +38,35 @@ func DevnetPreset() (*Preset, error) {
 	}
 
 	return &Preset{
-		Name: "Local Devnet",
-		ChainConfig: ChainConfig{
-			Preset:            DevnetPresetId,
-			L1Contracts:       l1Contracts,
-			L1PollingInterval: 5000,
-			L2PollingInterval: 5000,
-		},
+		Name:        "Local Devnet",
+		ChainConfig: ChainConfig{Preset: DevnetPresetId, L1Contracts: l1Contracts},
 	}, nil
 }
 
-// findMonorepoRoot will recursively search upwards for a go.mod file.
-// This depends on the structure of the monorepo having a go.mod file at the root.
-func findMonorepoRoot(startDir string) (string, error) {
+// findDevnetAllocs will recursively search upwards for a .devnet directory.
+// This depends on the structure of the monorepo generating the .devnet
+// folder on devnet-allocs
+func findDevnetAllocs(startDir string) (string, error) {
 	dir, err := filepath.Abs(startDir)
 	if err != nil {
 		return "", err
 	}
+
 	for {
-		modulePath := filepath.Join(dir, "go.mod")
+		modulePath := filepath.Join(dir, ".devnet")
 		if _, err := os.Stat(modulePath); err == nil {
 			return dir, nil
 		}
+
 		parentDir := filepath.Dir(dir)
+
 		// Check if we reached the filesystem root
 		if parentDir == dir {
 			break
 		}
+
 		dir = parentDir
 	}
-	return "", fmt.Errorf("monorepo root not found")
+
+	return "", fmt.Errorf(".devnet dir not found")
 }
